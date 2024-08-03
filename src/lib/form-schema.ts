6f61fdc53f835f000f5413323d1f907c90971243
Moch. Fariz Al Hazmi <mochfarizalhazmi@gmail.com>
2024-08-04 05:06:36 +0700

    Slicing Sign Up Page

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -9,6 +9,7 @@ export const signInFormSchema = z.object({
 })
 
 export const signUpFormSchema = z.object({
+  name: z.string({ required_error: 'Name is required' }),
   email: z
     .string({ required_error: 'Email is required' })
     .email({ message: 'Email is not valid' }),
